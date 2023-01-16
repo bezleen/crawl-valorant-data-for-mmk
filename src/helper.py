c70650dfb34897d62bd7f512f95154b75acc6bf5
hienhuynhdang <hienhuynh406@icloud.com>
2023-01-16 17:48:17 +0700

    random data

--- src/helper.py
+++ src/helper.py
@@ -4,6 +4,10 @@ sys.path.insert(0, '.')
 import copy
 import json
 import pydash as py_
+import csv
+import random
+from csv import DictWriter, writer
+from os.path import exists
 
 import src.url_config as URLConf
 import src.config as Conf
@@ -68,6 +72,77 @@ def generate_url():
     return
 
 
+def new_value(value, min=0, max=None):
+    # value = float(value)
+    assert max
+    percent = float(random.randint(5, 30))
+    plus_or_minus = random.randint(0, 1)
+    if plus_or_minus == 0:
+        new_value = value * ((100.0 - percent) / 100.0)
+        if new_value < min or new_value > max:
+            new_value = value * ((100.0 + percent) / 100.0)
+        return round(new_value, 3)
+    new_value = value * ((100.0 + percent) / 100.0)
+    if new_value < min or new_value > max:
+        new_value = value * ((100.0 - percent) / 100.0)
+    return round(new_value, 3)
+
+
+def random_data():
+
+    with open('data/valorant_mmk.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
+        header = True
+        for row in csv_reader:
+            if header:
+                header = False
+                continue
+            print(count)
+            count += 1
+
+            kd = float(row[0])
+            kill = float(row[1])
+            death = float(row[2])
+            assistant = float(row[3])
+            win_rate = float(row[4])
+            pick_rate = float(row[5])
+            avg_score = float(row[6])
+            first_blood_rate = float(row[7])
+            #
+            for _ in range(2):
+                kd = new_value(kd, max=1.0)
+                kill = new_value(kill, max=50)
+                death = new_value(death, max=50)
+                assistant = new_value(assistant, max=50)
+                win_rate = new_value(win_rate, max=100)
+                pick_rate = new_value(pick_rate, max=100)
+                avg_score = new_value(avg_score, max=1000)
+                first_blood_rate = new_value(first_blood_rate, max=10)
+                data = {
+                    "KD": kd,
+                    "Kill": kill,
+                    "Death": death,
+                    "Assistant": assistant,
+                    "WinRate": win_rate,
+                    "PickRate": pick_rate,
+                    "AvgScore": avg_score,
+                    "FirstBloodRate": first_blood_rate
+                }
+                file_path = 'data/extend_data.csv'
+                if not exists(file_path):
+                    with open(file_path, 'a') as f:
+                        writer_obj = writer(f)
+                        writer_obj.writerow(list(data.keys()))
+                        f.close()
+                with open(file_path, 'a') as f:
+                    dictwriter_obj = DictWriter(f, fieldnames=list(data.keys()))
+                    dictwriter_obj.writerow(data)
+                    f.close()
+
+
 if __name__ == '__main__':
     # generate_url()
-    generate_url_new()
+    # generate_url_new()
+
+    random_data()
